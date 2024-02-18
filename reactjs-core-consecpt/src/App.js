a96277a839eafac45a17039cca372becabb8615c
mintusikder <mintusikder15@gmail.com>
2024-02-18 18:10:33 +0600

    added using state increase and dectease

--- reactjs-core-consecpt/src/App.js
+++ reactjs-core-consecpt/src/App.js
@@ -1,30 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const products =[
-    {name:"Laptop",price:53000},
-    {name:"Phone",price:343000},
-    {name:"Watch",price:33000},
-    {name:"Tablet",price:73000},
-  ]
+  // const products =[
+  //   {name:"Laptop",price:53000},
+  //   {name:"Phone",price:343000},
+  //   {name:"Watch",price:33000},
+  //   {name:"Tablet",price:73000},
+  // ]
   return (
    <div>
-    {
+    {/* {
       products.map(product=> <Product name={product.name} price={product.price}></Product>)
-    }
+    } */}
     {/* <Product name="Laptop"></Product> */}
+
+
+    <Counter></Counter>
     </div>
   );
 }
 
-function Product(props){
+function Counter(){
+  const [count, setCount] = useState(33)
+  // console.log(abc)
+
+  // const increase = () =>{
+  //   const newCount= count + 1;
+  //   setCount(newCount)
+  // }
+
+  const increase = () => setCount(count +1)
+  const decrease = () => setCount(count -1)
+
   return(
-    <div className='product'>
-      <h3>Name: {props.name}</h3>
-      <p>Price: {props.price}</p>
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
     </div>
   )
 }
+
+
+// function Product(props){
+//   return(
+//     <div className='product'>
+//       <h3>Name: {props.name}</h3>
+//       <p>Price: {props.price}</p>
+//     </div>
+//   )
+// }
 
 export default App;
