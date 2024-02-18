02787d3c79e15cb8687d6389e6dc1c9ead92e54f
mintusikder <mintusikder15@gmail.com>
2024-02-18 17:32:56 +0600

    added prodect container

--- reactjs-core-consecpt/src/App.js
+++ reactjs-core-consecpt/src/App.js
@@ -2,18 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products =[
+    {name:"Laptop",price:53000},
+    {name:"Phone",price:343000},
+    {name:"Watch",price:33000},
+    {name:"Tablet",price:73000},
+  ]
   return (
    <div>
-    <Product></Product>
+    {
+      products.map(product=> <Product name={product.name} price={product.price}></Product>)
+    }
+    {/* <Product name="Laptop"></Product> */}
     </div>
   );
 }
 
-function Product(){
+function Product(props){
   return(
-    <div>
-      <h3>Name: </h3>
-      <p>Price: </p>
+    <div className='product'>
+      <h3>Name: {props.name}</h3>
+      <p>Price: {props.price}</p>
     </div>
   )
 }
