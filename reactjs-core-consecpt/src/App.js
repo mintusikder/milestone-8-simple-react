f029550530ed625907a1d334f95b775cf1a1ebcd
mintusikder <mintusikder15@gmail.com>
2024-02-18 17:18:12 +0600

    added react core consecpt application

---
+++ reactjs-core-consecpt/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div>
+    <Product></Product>
+    </div>
+  );
+}
+
+function Product(){
+  return(
+    <div>
+      <h3>Name: </h3>
+      <p>Price: </p>
+    </div>
+  )
+}
+
+export default App;
