5dc381d529ea4ee1405eb63faa3148d3d035658a
mintusikder <mintusikder15@gmail.com>
2024-02-18 13:56:01 +0600

    added props

--- new-react-app/src/App.js
+++ new-react-app/src/App.js
@@ -5,35 +5,39 @@ const textcenter = {
   textAlign: "center",
 };
 
+
+//simeler in loop different in data
 function App() {
   return (
     <div className="App">
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
+      <Person name="Bappa" nayeka="Moushumi"></Person>
+      <Person name= "Firoj vaiya" nayeka="Simi"></Person>
+      <Person nayeka="Porimoni"></Person>
       <h2 style={{ textAlign: "center" }}>My new friend</h2>
       <h2 style={textcenter}>rock mama React mama</h2>
-      <Friend></Friend>
+      <Friend name="Sajib" phone="017"></Friend>
+      <Friend name="Firoj Vai" phone="018"></Friend>
+      <Friend name="Saidul Vai" phone="019"></Friend>
     </div>
   );
 }
 
-function Person() {
+function Person(props) {
+  console.log(props)
   return (
     <div className="person">
-      <h1>Mintu Sikder</h1>
-      <p>Web Developer</p>
+      <h1>{props.name}</h1>
+      <p>Nayeka : {props.nayeka}</p>
     </div>
   );
 }
 
-function Friend() {
+function Friend(props) {
   return (
     <div className="component">
       <h1>All Friends</h1>
-      <p>Sajib</p>
-      <p>Rakib</p>
-      <p>Sakib</p>
+      <p>{props.name}</p>
+      <p>{props.phone}</p>
     </div>
   );
 }
