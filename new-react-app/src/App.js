6b4f141d1c2d1e107d4da04e5734e165bf0da085
mintusikder <mintusikder15@gmail.com>
2024-02-18 14:19:23 +0600

    added map for data

--- new-react-app/src/App.js
+++ new-react-app/src/App.js
@@ -5,14 +5,33 @@ const textcenter = {
   textAlign: "center",
 };
 
+const singers = [
+  {name:"Shakil", job:"singer"},
+  {name:"Riaj", job:"singer2"},
+  {name:"Atik", job:"singer3"},
+  {name:"Mintu", job:"singer4"},
+]
 
 //simeler in loop different in data
 function App() {
+  const nayoks = ["Bapparaj", "Kuber", "Mithun"];
   return (
     <div className="App">
-      <Person name="Bappa" nayeka="Moushumi"></Person>
-      <Person name= "Firoj vaiya" nayeka="Simi"></Person>
-      <Person nayeka="Porimoni"></Person>
+      {nayoks.map((nayok) => <li>Name={nayok}</li>
+      
+      
+      )}
+      {/* {nayoks.map((nayok) => <Person name={nayok}></Person>
+      
+      
+      )} */}
+
+      {
+        singers.map(singer => <Person name={singer.name}></Person>)
+      }
+      {/* <Person name={nayoks[0]} nayeka="Moushumi"></Person> */}
+      {/* <Person name="Firoj vaiya" nayeka="Simi"></Person>
+      <Person nayeka="Porimoni"></Person> */}
       <h2 style={{ textAlign: "center" }}>My new friend</h2>
       <h2 style={textcenter}>rock mama React mama</h2>
       <Friend name="Sajib" phone="017"></Friend>
@@ -23,11 +42,11 @@ function App() {
 }
 
 function Person(props) {
-  console.log(props)
+  console.log(props);
   return (
     <div className="person">
       <h1>{props.name}</h1>
-      <p>Nayeka : {props.nayeka}</p>
+      {/* <p>Nayeka : {props.nayeka}</p> */}
     </div>
   );
 }
